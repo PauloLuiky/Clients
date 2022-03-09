c5cb0dc316b32e74622f7dcdd638aac47c58cea1
PauloLuiky <paulo_shawn@htomail.com>
2022-03-09 15:11:45 -0300

    Script JS

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+const client1 = {
+  nome: 'Guilherme',
+  email: 'guilherme@test.com'
+}
+
+const client2 = {
+  nome: 'Paulo',
+  email: 'paulo@test.com'
+}
+
+const client3 = {
+  nome: 'Ricardo',
+  email: 'ricardo@test.com'
+}
+
+const list = [client1, client2, client3];
+*/
+
+/*
+let counter = 0;
+while(counter < 3) {
+  console.log(list[counter]);
+  counter ++;
+}
+*/
+/*
+for(let index = 0; index < 3; index++) {
+  console.log(list[index]);
+}
+*/
+
+const clients = [
+  {
+    nome: 'Guilherme',
+    email: 'guilherme@test.com'
+  },
+  {
+    nome: 'Paulo',
+    email: 'paulo@test.com'
+  },
+  {
+    nome: 'Ricardo',
+    email: 'ricardo@test.com'
+  }
+]
+
+const list = document.getElementById('clients');
+console.log(list);
